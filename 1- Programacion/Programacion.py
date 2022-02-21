c6edaf216423cfd60290cad79b776fbf3a6c627b
manuelpalomeque <pjonathanmanuel@hotmail.com>
2022-02-21 11:05:15 -0300

    Agregue consignas

---
+++ 1- Programacion/Programacion.py
@@ -0,0 +1,34 @@
+# 1) El primer desafío era hacer una función donde se analicen dos números y retorne
+# “true“ si el número2 era mayor al numero1, -1 si son iguales y “false” si numero1 es
+# mayor que numero2
+
+
+# 2) Desafío de cuerdas
+# Haga que la función StringChallenge( str ) tome el parámetro str que se pasa, que será
+# una cadena que contenga letras del alfabeto, y devuelva una nueva cadena según las
+# siguientes reglas. Cada vez que encuentre una M mayúscula, duplique el carácter
+# anterior (luego elimine la M), y cada vez que encuentre una N mayúscula, elimine el
+# siguiente carácter de la cadena (luego elimine la N). Todos los demás caracteres de la
+# cadena serán letras minúsculas. Por ejemplo: "abcNdgM" debería devolver "abcgg". La
+# cadena final nunca estará vacía.
+# Ejemplos
+# Entrada: "MrtyNNgMM"
+# Salida: rtyggg
+# Salida final: rtygggEntrada: "oMoMkkNrrN"
+# Salida: ooookkr
+#Salida final: ooookkr
+
+
+
+
+# 3) Desafío de cuerdas
+# Haga que la función StringChallenge( str ) tome el parámetro str y codifique el mensaje
+# de acuerdo con la siguiente regla: codifique cada letra en su posición numerada
+# correspondiente en el alfabeto. Los símbolos y espacios también se utilizarán en la
+# entrada. Por ejemplo: si str es "af5c a#!" ¡ entonces su programa debería
+# devolver 1653 1#! .
+# Ejemplos
+# Entrada: "hola 45"
+# Salida: 85121215 45Entrada: "jaj-a"
+# Salida: 10110-1
+
